e50cf94a36863db6af2be33dc8292b59b4f1d687
lh19 <lh19@FSTU603008.pti.edu>
2018-10-24 15:51:48 -0400

    This is the first commit

---
+++ src/ce4/CE4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ce4;
+
+/**
+ *
+ * @author lh19
+ */
+public class CE4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is Logan Hill, I will count to four.");
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
